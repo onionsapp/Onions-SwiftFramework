3d7e0f8af701fd41f93e191f3158f8e851071e17
Ben Gordon <bgordon@curse.com>
2015-12-14 11:22:52 -0600

    Initial Commit

---
+++ OnionsFramework/OnionsFramework.h
@@ -0,0 +1,19 @@
+//
+//  OnionsFramework.h
+//  OnionsFramework
+//
+//  Created by Benjamin Gordon on 12/14/15.
+//  Copyright © 2015 subvertllc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for OnionsFramework.
+FOUNDATION_EXPORT double OnionsFrameworkVersionNumber;
+
+//! Project version string for OnionsFramework.
+FOUNDATION_EXPORT const unsigned char OnionsFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OnionsFramework/PublicHeader.h>
+
+
