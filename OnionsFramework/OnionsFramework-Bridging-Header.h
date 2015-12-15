ba0b27b58cf2e5ac3bb845e9246ca0b9d93770cb
Ben Gordon <bgordon@curse.com>
2015-12-14 18:16:03 -0600

    Added bridging header

---
+++ OnionsFramework/OnionsFramework-Bridging-Header.h
@@ -0,0 +1,12 @@
+//
+//  OnionsFramework-Bridging-Header.h
+//  OnionsFramework
+//
+//  Created by Benjamin Gordon on 12/14/15.
+//  Copyright © 2015 subvertllc. All rights reserved.
+//
+
+#ifndef OnionsFramework_Bridging_Header_h
+#define OnionsFramework_Bridging_Header_h
+#import <Parse/Parse.h>
+#endif /* OnionsFramework_Bridging_Header_h */
